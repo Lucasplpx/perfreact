a6cd7722103cccb5d7c3d6c2ec020ceee99bd2c0
Lucas Passos <lucasplpx@gmail.com>
2022-01-19 20:20:27 -0300

    bundle analyzer

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,7 +1,7 @@
 import { memo, useState } from 'react';
 import { AddProductToWishlistProps } from './AddProductToWishlist';
-
 import dynamic from 'next/dynamic';
+import loadash from 'lodash';
 
 const AddProductToWishlist = dynamic<AddProductToWishlistProps>(
   () => {
@@ -50,6 +50,6 @@ export function ProductItemComponent({
 export const ProductItem = memo(
   ProductItemComponent,
   (prevProps, nextProps) => {
-    return Object.is(prevProps.product, nextProps.product);
+    return loadash.isEqual(prevProps.product, nextProps.product);
   }
 );
